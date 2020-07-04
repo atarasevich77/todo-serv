e41d304bab0e5bb6463ea8b9bcf7558d063ea265
kano-kenji <aleksandrtarasevich77@gmail.com>
2020-07-04 08:00:11 -0600

    try connect to db from heroku

--- src/modules/core/db.js
+++ src/modules/core/db.js
@@ -6,7 +6,6 @@ export default function mongoConnection() {
     // 'mongodb://admin:19vilka76@cluster0-shard-00-00-4mcts.mongodb.net:27017,cluster0-shard-00-01-4mcts.mongodb.net:27017,cluster0-shard-00-02-4mcts.mongodb.net:27017/todo?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority',
     `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PWD}@${process.env.MONGO_HOST}/${process.env.MONGO_DB_NAME}`,
     { useNewUrlParser: true },
-    console.log(process.env.MONGO_USER)
   );
 
   mongoose.connection.on('error', () => {
